934b3db61b111a6d49aa0fa3e92b3d5d4fabbbb1
Dibar <dibux9@gmail.com>
2022-06-26 10:52:37 -0400

    0

---
+++ src/day28_encapsulation/encapsulation/Person.java
@@ -0,0 +1,32 @@
+package day28_encapsulation.encapsulation;
+
+public class Person {
+
+
+
+    /*
+    Variables:
+
+    name, age, gender, dateOfBirth
+
+    name:
+        write: cannot be null/empty/blank
+        read: if name is null, return "Unknown"
+
+    age:
+        write: age cannot be less than 0 or greater than 120
+
+    gender:
+        write: gender only be 'M' or 'F'
+
+    dateOfBirth:
+        write: cannot be after today's date
+
+    Add a constructor that can set all the fields
+
+    Add toString()
+
+
+
+     */
+}
