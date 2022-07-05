817c149cb1db84855b7816cebf028ca94e97fe50
Dibar <dibux9@gmail.com>
2022-07-05 11:07:40 -0400

    0

---
+++ src/day33_abstraction/employeeTask/TestEmployeeObjects.java
@@ -0,0 +1,30 @@
+package day33_abstraction.employeeTask;
+
+public class TestEmployeeObjects {
+
+    public static void main(String[] args) {
+
+        // Employee employee = new Employee("Daniel", 54, 'M', "A1", "SDET", 85000);
+        // WE cannot create objects from an abstract class, object must be created from a concrete class
+
+        Teacher teacher = new Teacher("James", 45, 'M', "B1", "Math teacher", 75000);
+
+        Developer developer = new Developer("Lucy", 30, 'F', "C1", "Java Developer", 95000, "Java");
+
+        Driver driver = new Driver("Aeron", 48, 'M', "D1", "Truck Driver", 90000);
+
+        Tester tester = new Tester("Emily", 35, 'F', "E1", "Manual Tester", 80000);
+
+        System.out.println(teacher);
+        System.out.println(developer);
+        System.out.println(tester);
+        System.out.println(driver);
+
+        System.out.println("----------------------------------------------");
+
+        teacher.work();
+        developer.work();
+        tester.work();
+        driver.work();
+    }
+}
